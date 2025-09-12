55f1241d6cd8e436ecab4f45fa40accdc3e37f04
SabirHossen339 <sabirshimanto01729@gmail.com>
2025-09-12 12:09:55 +0600

    Error Fixed

--- JS/index.js
+++ JS/index.js
@@ -14,6 +14,17 @@ const displayLevelWord = (words) => {
   const wordContainer = document.getElementById("word-container");
   wordContainer.innerHTML = "";
 
+
+  if (words.length == 0) {
+    wordContainer.innerHTML = `
+<div class="text-center col-span-full space-y-4 ">
+<img class="mx-auto" src="./assets/alert-error.png" />
+      <p class="bangla-2 text-2xl font-medium text-gray-700">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+      <h2 class="bangla-2 text-4xl font-bold ">নেক্সট Lesson এ যান</h2>
+    </div>
+   
+   `;
+  }
   //   {
   //     "id": 82,
   //     "level": 1,
@@ -27,9 +38,9 @@ const displayLevelWord = (words) => {
     const card = document.createElement("div");
     card.innerHTML = `
     <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
-      <h2 class="font-bold text-2xl">${word.word}</h2>
+      <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
       <p class="font-semibold">Meaning /Pronounciation</p>
-      <div class="bangla-2 font-medium text-2xl">"${word.meaning} / ${word.pronunciation}"</div>
+      <div class="bangla-2 font-medium text-2xl">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "উচ্চারণ পাওয়া যায়নি"}"</div>
       <div class="flex justify-between items-center">
         <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
         <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></i></button>
